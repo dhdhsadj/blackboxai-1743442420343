1ba97ab58527244a18b36e1a1eca3a927579baee
Diogo Oliveira5 <168922331+dhdhsadj@users.noreply.github.com>
2025-03-31 18:33:56 +0100

    Add biometric_time_tracker/lib/screens/main_navigation.dart 1743442435781

---
+++ biometric_time_tracker/lib/screens/main_navigation.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:biometric_time_tracker/screens/home_screen.dart';
+import 'package:biometric_time_tracker/screens/employees_screen.dart';
+import 'package:biometric_time_tracker/screens/reports_screen.dart';
+import 'package:biometric_time_tracker/screens/company_profile_screen.dart';
+import 'package:biometric_time_tracker/constants.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const EmployeesScreen(),
+    const ReportsScreen(),
+    const CompanyProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: AppColors.primary,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Employees',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: 'Reports',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
